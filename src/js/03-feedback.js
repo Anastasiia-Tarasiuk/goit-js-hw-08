7b5fa7f7f4eb56e3afd60f3de2b154c9c99f8316
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-05-06 18:21:51 +0300

    Виконує завдання 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,44 @@
 const formEl = document.querySelector(".feedback-form");
-console.log()
+const emailEl = document.querySelector("input");
+const messageEl = document.querySelector("textarea");
+
+const STORAGE_KEY = "feedback-form-state";
+
+formEl.addEventListener('input', onInput);
+
+function onInput (event) {
+    const formData = new FormData(event.currentTarget);
+ 
+    const objData = {};
+    
+    formData.forEach((value, name) => {
+        objData[name] = value;
+    });
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(objData));
+}
+
+window.onload = load;
+
+function load() {
+    const objFromStorage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    
+    if (localStorage.getItem(STORAGE_KEY)) {
+        emailEl.value = objFromStorage.email;
+        messageEl.value = objFromStorage.message;            
+    } else {
+        emailEl.value = "";
+        messageEl.value = "";
+    }
+}
+      
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+
+    localStorage.removeItem("feedback-form-state");
+    formEl.reset();
+}
