c7abb08cc466c213a354e9d835d17f67254c5dfc
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-05-06 16:42:12 +0300

    Вирішує завдання 1 та 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// ======= завдання ==========
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+function createMarkup(items) {
+    
+    return items.map(item => {
+        const { preview, original, description } = item;
+        return `<li><a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a></li> `;
+    }).join('');
+    
+}
+
+const gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250 });
+gallery.on('show.simplelightbox', function () {   
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Vimeo from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+const onPlay = function(data) {
+   let currentTime = data.seconds;
+    
+   localStorage.setItem("videoplayer-current-time", currentTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
